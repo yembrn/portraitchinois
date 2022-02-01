1cb4b6a8c43ecad3fbc22f959d0e493b445b8389
yembrn <98811138+yembrn@users.noreply.github.com>
2022-02-01 12:13:56 +0100

    Add files via upload

---
+++ portrait-chinois/READ.md
@@ -0,0 +1,4 @@
+N'étant pas parvenu à créer le fichier READ.md j'ai été contrainte de mettre le lien ci-contre.
+
+
+https://etudiant.u-pem.fr/~emy.bigirimana/portrait-chinois/
---
+++ portrait-chinois/script.js
@@ -0,0 +1,100 @@
+// CodePen: https://codepen.io/ettrics/pen/WRbGRN //
+class StickyNavigation {
+	
+	constructor() {
+		this.currentId = null;
+		this.currentTab = null;
+		this.tabContainerHeight = 70;
+		let self = this;
+		$('.et-hero-tab').click(function() { 
+			self.onTabClick(event, $(this)); 
+		});
+		$(window).scroll(() => { this.onScroll(); });
+		$(window).resize(() => { this.onResize(); });
+	}
+	
+	onTabClick(event, element) {
+		event.preventDefault();
+		let scrollTop = $(element.attr('href')).offset().top - this.tabContainerHeight + 1;
+		$('html, body').animate({ scrollTop: scrollTop }, 600);
+	}
+	
+	onScroll() {
+		this.checkTabContainerPosition();
+    this.findCurrentTabSelector();
+	}
+	
+	onResize() {
+		if(this.currentId) {
+			this.setSliderCss();
+		}
+	}
+	
+	checkTabContainerPosition() {
+		let offset = $('.et-hero-tabs').offset().top + $('.et-hero-tabs').height() - this.tabContainerHeight;
+		if($(window).scrollTop() > offset) {
+			$('.et-hero-tabs-container').addClass('et-hero-tabs-container--top');
+		} 
+		else {
+			$('.et-hero-tabs-container').removeClass('et-hero-tabs-container--top');
+		}
+	}
+	
+	findCurrentTabSelector(element) {
+		let newCurrentId;
+		let newCurrentTab;
+		let self = this;
+		$('.et-hero-tab').each(function() {
+			let id = $(this).attr('href');
+			let offsetTop = $(id).offset().top - self.tabContainerHeight;
+			let offsetBottom = $(id).offset().top + $(id).height() - self.tabContainerHeight;
+			if($(window).scrollTop() > offsetTop && $(window).scrollTop() < offsetBottom) {
+				newCurrentId = id;
+				newCurrentTab = $(this);
+			}
+		});
+		if(this.currentId != newCurrentId || this.currentId === null) {
+			this.currentId = newCurrentId;
+			this.currentTab = newCurrentTab;
+			this.setSliderCss();
+		}
+	}
+	
+	setSliderCss() {
+		let width = 0;
+		let left = 0;
+		if(this.currentTab) {
+			width = this.currentTab.css('width');
+			left = this.currentTab.offset().left;
+		}
+		$('.et-hero-tab-slider').css('width', width);
+		$('.et-hero-tab-slider').css('left', left);
+	}
+	
+}
+
+new StickyNavigation();
+// Fin du CodePen //
+
+//FORMULAIRE//
+document.getElementById('formulaire').addEventListener('click', function() {
+    var blocformulaire = document.createElement("div");
+    blocformulaire.innerHTML =
+        '<h2>Si j\'étais ' + document.getElementById('si').value + ' je serais ' + document.getElementById('jeserais').value + '</h2>' +
+        '<div class="bloc"><img alt="image" src="' + document.getElementById('illu').value + '" class="img-right">' +
+        '<section>' + document.getElementById('car').value + '</section><br>' +
+        '</div>' +
+        '<br>' +
+        '<br>';
+    document.querySelector("main").append(blocformulaire);
+})
+    //Mentions Légales
+   // $('#reveal-click').click(function () {
+     //   $('#volet-invisible #hidden-div').slideToggle({
+      //  direction: "up"
+       // }, 300);
+       // $(this).toggleClass('clientsClose');
+    //}); // end click
+
+
+
